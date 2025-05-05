314d0d0b610f7cb2e5f5d57cda38e0465f4c2fcb
PajaspaceNet <elektrosoft.paja@gmail.com>
2025-05-05 09:40:05 +0200

    Update LoginSteps.java

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -6,23 +6,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginSteps {
+
     WebDriver driver;
 
     @Given("I am on the login page")
-    public void openLoginPage() {
+    public void i_am_on_the_login_page() {
         driver = new ChromeDriver();
-        driver.get("https://example.com/login");
+        driver.get("https://example.com/login"); // nahraď podle potřeby
     }
 
-    @When("I enter valid username and password")
-    public void enterCredentials() {
+    @When("I enter valid credentials")
+    public void i_enter_valid_credentials() {
         driver.findElement(By.id("username")).sendKeys("testuser");
         driver.findElement(By.id("password")).sendKeys("testpass");
         driver.findElement(By.id("login")).click();
     }
 
     @Then("I should see the dashboard")
-    public void verifyDashboard() {
+    public void i_should_see_the_dashboard() {
         assert driver.getCurrentUrl().contains("dashboard");
         driver.quit();
     }
